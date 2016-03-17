1ea3b22d36f8624bff997e21c6aa4ec182ce0f06
Russell Pate <russ@russpate.com>
2016-03-17 10:38:55 -0400

    all pretty and styled

--- js/templates.js
+++ js/templates.js
@@ -1,7 +1,7 @@
 module.exports = {
   newBitterForm: [
     '<div class="create-bitter-form-wrap">',
-      '<h1>Bitter</h1>',
+      '<h1>Bitters</h1>',
         '<form class="create-bitter-form">',
           '<input type="text" class="create-bitter-input-name" placeholder="username">',
           '<input type="text" class="create-bitter-input-post"placeholder="what\'s crappening?" maxlength="144">',
